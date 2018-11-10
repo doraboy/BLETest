8dec97722a46df49e439f39cce44d6f05e7fcdc1
kleinerQ <chih.yen5408@gmail.com>
2018-11-10 10:27:36 +0800

    v1

---
+++ app/src/main/java/tw/dora/bletest/BLEService.java
@@ -0,0 +1,38 @@
+package tw.dora.bletest;
+
+import android.app.Service;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
+import android.content.Context;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class BLEService extends Service {
+    private BluetoothAdapter mBluetoothAdapter;
+    private BluetoothManager bluetoothManager;
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        final BluetoothManager bluetoothManager =
+                (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        mBluetoothAdapter = bluetoothManager.getAdapter();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+}
